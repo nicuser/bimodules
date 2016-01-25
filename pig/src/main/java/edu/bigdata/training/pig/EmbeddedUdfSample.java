070fdf17d86695bbf23577458c3769fe253a3d3d
Laxmikanth Samudrala <myhome@Laxmikanths-MBP.hsd1.pa.comcast.net>
2016-01-24 19:45:32 -0500

    added kafka, pig modules

---
+++ pig/src/main/java/edu/bigdata/training/pig/EmbeddedUdfSample.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.bigdata.training.pig;
+
+/**
+ *
+ * @author lsamud001c
+ */
+import java.io.IOException;
+import org.apache.pig.PigServer;
+
+public class EmbeddedUdfSample {
+
+    public static void main(String a[]) throws Exception {
+        PigServer pigServer = new PigServer("local");
+
+        try {
+            //pigServer.registerJar("udfRegistry/pigUdf-1.0-SNAPSHOT.jar");
+            runMyQuery(pigServer, "src/main/resources/myinput.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runMyQuery(PigServer pigServer, String inputFile) throws IOException {
+        pigServer.registerQuery("A = load '" + inputFile + "' using TextLoader();");
+        pigServer.registerQuery("B = foreach A generate flatten(TOKENIZE($0));");
+        pigServer.registerQuery("C = group B by $0;");
+        pigServer.registerQuery("D = foreach C generate flatten(group), COUNT(B.$0);");
+        pigServer.registerQuery("E = FILTER D BY $1 > 1;");
+        
+        pigServer.store("E", "myoutput");
+    }
+}
